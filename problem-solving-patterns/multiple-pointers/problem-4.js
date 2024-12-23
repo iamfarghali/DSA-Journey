ff1d32cb24513a6da22ee2f5aa0b33062f2c9cdd
Farghali <iamfarghali@gmail.com>
2024-12-23 22:23:17 +0200

    working on problem-4 Count Distinct Pairs with Sum

---
+++ problem-solving-patterns/multiple-pointers/problem-4.js
@@ -0,0 +1,26 @@
+// =============================================================
+/*
+  Write a function called countPairsWithSum that accepts a sorted array of integers and a target sum, and returns the number of distinct pairs of values in the array that add up to the target sum.
+
+  Ex:
+    countPairsWithSum([1, 1, 2, 3, 3, 4, 5], 6);
+    // Output: 2
+    // Explanation: The pairs are (1, 5) and (2, 4).
+
+    countPairsWithSum([1, 2, 3, 4, 5], 10);
+    // Output: 0
+    // Explanation: No pairs add up to 10.
+
+    countPairsWithSum([1, 2, 2, 3, 3, 4, 5], 5);
+    // Output: 2
+    // Explanation: The pairs are (1, 4) and (2, 3).
+
+
+*/
+// =============================================================
+
+// =============================================================
+// SOLUTION
+// =============================================================
+
+function countPairsWithSum(arr, target) {}

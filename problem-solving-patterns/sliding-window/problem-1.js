a50e91b990da03cb320673031142fad66e32e155
Farghali <iamfarghali@gmail.com>
2024-12-26 22:11:05 +0200

    wip

--- problem-solving-patterns/sliding-window/problem-1.js
+++ problem-solving-patterns/sliding-window/problem-1.js
@@ -21,22 +21,18 @@ function maxSubarraySum(arr, n) {
   let max = -Infinity;
 
   for (let i = 0; i < arr.length; i++) {
-    let sum = arr[i];
     let stop = n + i;
-
     if (stop >= arr.length) {
       const diff = stop - arr.length;
       stop = stop - (diff + 1);
     }
 
-    if (i < arr.length - 1) {
-      for (let j = i + 1; j < stop; j++) {
-        sum += arr[j];
-      }
+    let sum = arr[i];
+    for (let j = i + 1; j < stop; j++) {
+      sum += arr[j];
     }
     max = Math.max(max, sum);
   }
-
   return max;
 }
 

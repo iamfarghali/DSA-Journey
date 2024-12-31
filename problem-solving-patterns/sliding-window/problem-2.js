bae95842c81a6400b9ab90fa8aa9c91684c6f7c4
Farghali <iamfarghali@gmail.com>
2024-12-31 22:00:53 +0200

    wip

---
+++ problem-solving-patterns/sliding-window/problem-2.js
@@ -0,0 +1,13 @@
+// =============================================================
+/*
+  Write a function to find the length of the smallest contiguous subarray whose sum is greater than or equal to a given value target. If no such subarray exists, return 0.
+
+  Ex:
+    minSubarrayLength([2, 3, 1, 2, 4, 3], 7)  // 2
+
+*/
+// =============================================================
+
+// =============================================================
+// SOLUTION
+// =============================================================

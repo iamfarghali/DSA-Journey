9ae5305ac567f90a9464963dacd1e893d9bd7be8
Farghali <iamfarghali@gmail.com>
2025-02-07 23:56:42 +0200

    add misc for adding daily random problems

---
+++ misc/problem-1.js
@@ -0,0 +1,19 @@
+// =============================================================
+/*
+  [Longest Subarray with Sum at Most K]
+
+  Given an array of positive integers nums and an integer K, find the length of the longest contiguous subarray whose sum is at most K.
+
+
+  - Examples:
+    longestSubarrayAtMostK([3, 1, 2, 1, 4, 5], 6)  // 3 -> [3, 1, 2]
+    longestSubarrayAtMostK([3, 1, 2, 1, 1, 4, 5], 6)  // 4 -> [1, 2, 1, 1]
+  
+*/
+// =============================================================
+
+// =============================================================
+// SOLUTION
+// =============================================================
+
+function longestSubarrayAtMostK(nums, k) {}

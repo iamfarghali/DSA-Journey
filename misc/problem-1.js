ab99d662f8c38fc84295129f0c8fc578b861c4df
Farghali <iamfarghali@gmail.com>
2025-02-08 21:39:43 +0200

    solve problem-1 Longest Subarray with Sum at Most K

--- misc/problem-1.js
+++ misc/problem-1.js
@@ -16,4 +16,23 @@
 // SOLUTION
 // =============================================================
 
-function longestSubarrayAtMostK(nums, k) {}
+function longestSubarrayAtMostK(nums, k) {
+  let left = -1,
+    longest = 0,
+    sum = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+    while (sum > k) {
+      left++;
+      sum -= nums[left];
+    }
+    longest = Math.max(longest, right - left);
+  }
+
+  return longest;
+}
+
+console.log(longestSubarrayAtMostK([3, 1, 2, 1, 4, 5], 6)); // 3
+console.log(longestSubarrayAtMostK([3, 1, 2, 1, 1, 4, 5], 6)); // 4
+console.log(longestSubarrayAtMostK([], 6)); // 0

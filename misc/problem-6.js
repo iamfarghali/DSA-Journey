c8aedcc202b665e8b21cf4440a76dcc8c4da07f1
Farghali <iamfarghali@gmail.com>
2025-02-22 23:31:05 +0200

    wip problem-6 Longest Substring with At Most K Distinct Characters

---
+++ misc/problem-6.js
@@ -0,0 +1,25 @@
+// =============================================================
+/*
+  [Longest Substring with At Most K Distinct Characters]
+
+  Given a string s and an integer k, return the length of the longest substring of s that contains at most k distinct characters.
+
+  - Constraints:
+    * 0 <= s.length <= 5 * 10^4
+    * 0 <= k <= 50  
+  - Examples:
+    1. s = "eceba", k = 2 => Output: 3
+    2. s = "aa", k = 1 => Output: 2
+    3. s = "a", k = 1 => Output: 1
+    4. s = "ab", k = 1 => Output: 1
+    5. s = "ab", k = 2 => Output: 2
+    6. s = "ab", k = 3 => Output: 2
+    7. s = "abc", k = 1 => Output: 1
+*/
+// =============================================================
+
+// =============================================================
+// SOLUTION
+// =============================================================
+
+function lengthOfLongestSubstringKDistinct(s, k) {}
